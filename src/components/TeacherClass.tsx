f41fa826f2d87c0c85be1ea1040d7ffad7c8fd43
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-08-29 22:34:22 +0530

    Teacher Done

---
+++ src/components/TeacherClass.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import CreateClassDialog from "@/components/CreateClassDialog";
+import { Button } from "@/components/ui/button";
+import ClassCards from "@/components/DisplayClass";
+
+export default function TeacherClass() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
+  const handleOpen = () => {
+    setDialogOpen(true);
+  };
+
+  return (
+    <div className="container mx-auto p-4 ">
+      <h1 className="text-3xl font-bold mb-6">Welcome to the Class Management System</h1>
+      <Button onClick={handleOpen} className="btn-primary">
+        Create Class
+      </Button >
+      {dialogOpen && <CreateClassDialog onClose={handleClose} />}
+      <div className="p-4">
+        <ClassCards />
+      </div>
+    </div>
+  );
+}
